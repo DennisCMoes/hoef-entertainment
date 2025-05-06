794dabf36382cb7ce72814543c03e0b786766b79
Dennis Moes <dennismoes@me.com>
2025-05-06 09:31:22 +0200

    Changed to local storage

--- keystatic.config.ts
+++ keystatic.config.ts
@@ -2,8 +2,7 @@ import { config, fields, collection } from '@keystatic/core'
 
 export default config({
   storage: {
-    kind: 'github',
-    repo: 'DennisCMoes/hoef-entertainment',
+    kind: 'local',
   },
   collections: {
     posts: collection({
